7acf33bd861dc3e8b779d22a21837cf7e718e2d3
Meryl Turner <merylturner08@gmail.com>
2017-07-27 11:54:13 -0700

    failing validation test passing

--- tests/unit/restaurants.test.js
+++ tests/unit/restaurants.test.js
@@ -13,9 +13,18 @@ describe('restaurant model', () => {
                 zip: 80908
             }
         });
-
         return restaurant.validate();
+    });
 
+    it('fails validation if required fields missing', () => {
+        const restaurant = new Restaurant();
+        return restaurant.validate()
+            .then(
+                () => { throw new Error('expected validation error');},
+                ({ errors }) => {
+                    assert.ok(errors.name);
+                }
+            )
     });
     
 });
