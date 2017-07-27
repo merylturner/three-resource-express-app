c6ab7805a00d66af180af4000cdd370a3049b89a
Meryl Turner <merylturner08@gmail.com>
2017-07-27 09:53:08 -0700

    enum type test passing

--- tests/unit/taco.test.js
+++ tests/unit/taco.test.js
@@ -15,13 +15,27 @@ describe('Taco model', () => {
 
     it('fails validation if required fields missing', () => {
         const taco = new Taco();
-
         return taco.validate()
             .then(
                 () => { throw new Error('expected validation error');},
                 ({ errors }) => {
                     assert.ok(errors.name);
                     assert.ok(errors.type);
+                }
+            );
+    });
+
+    it('type should be of enum type', () => {
+        const taco = new Taco({
+            name: 'foo',
+            type: 'bacon'
+        });
+
+        return taco.validate()
+            .then(
+                () => { throw new Error('expected validation error');},
+                ({ errors }) => {
+                    assert.equal(errors.type.kind, 'enum');
                 }
             );
     });
