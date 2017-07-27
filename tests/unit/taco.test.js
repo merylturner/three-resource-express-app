cde24ee1715757f6e0f1f3cc4204b9bf5df4a12c
Meryl Turner <merylturner08@gmail.com>
2017-07-27 09:25:58 -0700

    first test validation passing

--- tests/unit/taco.test.js
+++ tests/unit/taco.test.js
@@ -1,10 +1,14 @@
 const Taco = require('../../lib/models/taco');
-const { assert } = require('chai');
+// const { assert } = require('chai');
 
 describe('Taco model', () => {
+    
     it('validates the taco', () => {
         const taco = new Taco({
-            
+            name: 'mucho spicy',
+            type: 'pork',
+            cheese: true,
+            toppings: 'lettuce'
         });
         return taco.validate();
     });
