579b71005ce8607e824024b246bcf76edd14ca04
Meryl Turner <merylturner08@gmail.com>
2017-08-02 11:30:12 -0700

    invalid token test passing

--- tests/e2e/auth.test.js
+++ tests/e2e/auth.test.js
@@ -15,11 +15,6 @@ const request = chai.request(app);
 describe('auth', () => {
     before(() => connection.dropDatabase());
 
-    // before((user = { email: 'meryl@meryl.com', password: '123' }) => {
-    //     return request.post('/auth/signup')
-    //         .send(user)
-    //         .then(res => res.body.token);
-    // });
 
     const user = {
         email: 'user',
@@ -27,7 +22,14 @@ describe('auth', () => {
     };
 
     describe('user management', () => {
+        let token = '';
 
+        // before(() => {
+        //     return request.post('/auth/signup')
+        //         .send({ email: 'meryl@meryl.com', password: '123' })
+        //         .then(res => token = res.body.token);
+        // });
+        
         const badRequest = (url, data, code, error) => {
             request
                 .post(url)
@@ -40,12 +42,53 @@ describe('auth', () => {
                 assert.equal(res.response.body.error, error);
             };
         };
+
         it('signup requires an email', () => {
-            badRequest('/auth/signup', { password: '123'}, 400, 'email and password must be supplied');
+            badRequest('/auth/signup', { password: '123' }, 400, 'email and password must be supplied');
         });
 
         it('signup requires a password', () => {
-            badRequest('/auth/signup', { email: 'user'}, 400, 'email and password must be supplied');
+            badRequest('/auth/signup', { email: 'user' }, 400, 'email and password must be supplied');
         });
+
+
+        it('user signup', () => {
+            request
+                .post('/auth/signup')
+                .send(user)
+                .then(res => assert.ok(token = res.body.token));
+        });
+
+        it('cannot use the same email', () => {
+            badRequest('/auth/signup', user, 400, 'email already in use');
+        });
+
+        it('sign in with wrong user', () => {
+            badRequest('/auth/signup', { email: 'bad user', password: user.password }, 400, 'Invalid Login');
+        });
+
+        it('sign in with wrong password', () => {
+            badRequest('/auth/signup', { email: user.email, password: 'bad' }, 400, 'Invalid Login');
+        });
+
+        it('user signup', () => {
+            request
+                .post('/auth/signup')
+                .send(user)
+                .then(res => assert.ok(res.body.token));
+        });
+
+        it('invalid token', () => {
+            request
+                .get('/auth/verify')
+                .set('Authorization', 'bad token')
+                .then(() => {
+                    throw new Error('successful response not expected');
+                },
+                res => {
+                    assert.equal(res.status.code, 401);
+                });
+        });
+
     });
 });
