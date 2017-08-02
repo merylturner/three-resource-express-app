5ca7ee4baac5184b05a128d90d70437e2d193c18
Meryl Turner <merylturner08@gmail.com>
2017-08-02 16:15:36 -0700

    moved over to db and request files, current tests still passing

--- tests/e2e/drinks.test.js
+++ tests/e2e/drinks.test.js
@@ -1,19 +1,10 @@
-const chai = require('chai');
-const assert = chai.assert;
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
+const db = require('./helpers/db');
+const request = require('./helpers/request');
+const { assert } = require('chai');
 
-process.env.MONGODB_URI = 'mongodb://localhost:27017/drinks-test';
-
-require('../../lib/connect');
-
-const connection = require('mongoose').connection;
-
-const app = require('../../lib/app');
-const request = chai.request(app);
 
 describe('drinks REST api', () => {
-    before(() => connection.dropDatabase());
+    before(db.drop);
 
     const margarita = {
         name: 'margarita',
--- tests/e2e/tacos.test.js
+++ tests/e2e/tacos.test.js
@@ -1,19 +1,9 @@
-const chai = require('chai');
-const assert = chai.assert;
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
-
-process.env.MONGODB_URI = 'mongodb://localhost:27017/tacos-test';
-
-require('../../lib/connect');
-
-const connection = require('mongoose').connection;
-
-const app = require('../../lib/app');
-const request = chai.request(app);
+const db = require('./helpers/db');
+const request = require('./helpers/request');
+const { assert } = require('chai');
 
 describe('tacos REST api', () => {
-    before(() => connection.dropDatabase());
+    before(db.drop);
 
     const nomNom = {
         name: 'nomNom',
