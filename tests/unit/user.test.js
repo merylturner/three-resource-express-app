9bc94d01f750b4e5f9fc9a4aea4833556c4e4373
Meryl Turner <merylturner08@gmail.com>
2017-08-02 09:39:39 -0700

    user generate hash passing

---
+++ tests/unit/user.test.js
@@ -0,0 +1,16 @@
+const User = require('../../lib/models/user');
+const { assert } = require('chai');
+
+describe('user model', () => {
+
+    it('generates hash for new user', () => {
+        const user = new User({
+            email: 'meryl@meryl.com'
+        });
+        const password = '123';
+
+        user.generateHash(password);
+        assert.notEqual(user.hash, password);
+
+    });
+});
